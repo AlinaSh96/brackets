180a89f5463ace5b773be327ec50cd6eb3e8927f
AlinaSh96 <alina30081996@gmail.com>
2020-10-24 22:20:15 +0300

    feat: add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+  
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let obj = {};
+  let brackets = [];
+  let arr = str.split("");
+  for(let i = 0; i < bracketsConfig.length; i++) {
+      obj[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+  for(let i = 0; i < arr.length; i++) {
+      if(arr[i] === (brackets[brackets.length-1] || 0)) {
+          brackets.pop();
+      }
+      else {
+          brackets.push(obj[arr[i]]);
+      }
+  }
+  return !brackets.length;
 }
